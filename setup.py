56bbab28c37225f54c9b28788e4cd16a478764af
wvxvw <olegsivokon@gmail.com>
2013-10-02 09:34:22 +0300

    Fixed some typos, expanded the README.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name             = 'walla-test-assignment',
+setup(name             = 'intj',
       version          = '0.0.1',
       author           = 'Oleg Sivokon',
       author_email     = 'olegsivokon@gmail.com',
       packages         = find_packages(),
       license          = 'LICENSE',
-      url              = 'http://wvxvw.github.io/walla-test-assignment',
+      url              = 'https://github.com/wvxvw/intj',
       description      = 'A toy social network site.',
       long_description = open('README.org').read(),
-      install_requires = ['tornado >=  3.1.1'])
+      install_requires = ['tornado >=  3.1.1', 'py2neo >= 1.6.0'])
